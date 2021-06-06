ac01b2ce6cdcaf31ef75bdaeaa85de696b94683a
AlexandrOkushko <sevenr23@gmail.com>
2021-06-06 22:12:39 +0300

    Add project files.

---
+++ CafeDAL.TestDriver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CafeDAL.TestDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
