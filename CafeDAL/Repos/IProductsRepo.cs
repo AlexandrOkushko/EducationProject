247cb61ea433fde00476565979f645b473438391
AlexandrOkushko <sevenr23@gmail.com>
2021-07-22 23:44:20 +0300

    add product repo

--- CafeDAL/Repos/IProductsRepo.cs
+++ CafeDAL/Repos/IProductsRepo.cs
@@ -6,7 +6,7 @@ namespace CafeDAL.Repos
     public interface IProductsRepo : IRepo<Products>
     {
         List<Products> Search(string searchString);
-        List<Products> GetProductsBySupplierId();
+        List<Products> GetProductsBySupplierId(int id);
         List<Products> GetRelatedData();
     }
 }
---
+++ CafeDAL/Repos/ProductsRepo.cs
@@ -0,0 +1,25 @@
+﻿using CafeDAL.EF;
+using CafeDAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using static Microsoft.EntityFrameworkCore.EF;
+
+namespace CafeDAL.Repos
+{
+    public class ProductsRepo : BaseRepo<Products>, IProductsRepo
+    {
+        public ProductsRepo(CafeContext context) : base(context)
+        {
+        }
+
+        public List<Products> GetProductsBySupplierId(int id)
+        => Context.Products.Where(c => c.SupplierId == id).ToList(); // 1 = Main supplier
+
+        public List<Products> GetRelatedData()
+            => Context.Products.FromSqlInterpolated($"SELECT * FROM Products").ToList();
+
+        public List<Products> Search(string searchString)
+            => Context.Products.Where(c => Functions.Like(c.Name, $"%{searchString}%")).ToList();
+    }
+}
